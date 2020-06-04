552c2ed054b859abc0ec307b02830bb79fee1520
Ayman Nait Cherif <ayman.nait21@gmail.com>
2020-06-04 16:21:07 +0100

    README.md updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Cinema reservations app with Angular and spring boot 
 
+![Screen 1](screens/s1.jpg)
+* 
+![Screen 2](screens/s2.jpg)
---
+++ back/src/test/java/com/anc/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anc.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
